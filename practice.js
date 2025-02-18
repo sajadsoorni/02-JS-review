3ecc76f44543355cd4f2bed87070f053fdbfcf68
Sajad <sajadsoorni@gmail.com>
2025-02-18 18:57:54 +0000

    Sort object by values in descending order

--- practice.js
+++ practice.js
@@ -2,3 +2,15 @@ const arr = [2, 3, 0];
 
 const basket = arr?.[1] ?? 'Out of list';
 basket;
+
+//------------------------------------------------
+
+const data = { Sara: 1, Amir: 4, Eli: 2, a: 21, wiki: 3 };
+
+const sortWordCount = Object.entries(data)
+  .sort(([, valueA], [, valueB]) => valueB - valueA)
+  .reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+console.log(sortWordCount);
